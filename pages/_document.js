85cc0db83d5b070ee256fdf2750216340cf44cf1
carlodp <santos.carlodp@gmail.com>
2023-03-14 02:01:04 +0800

    crossOrigin update

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin="true"
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&family=Pacifico&display=swap"
