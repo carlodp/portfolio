f5685a9baca8a16f73ee56c47292217fe2c49718
carlodp <santos.carlodp@gmail.com>
2023-03-14 01:09:12 +0800

    font update

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&family=Pacifico&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
